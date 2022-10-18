d1931dd82ab62b3f34b3954a2a5c4d1017dc639e
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2022-10-18 18:18:14 -0300

    Req12 - Feito

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,15 +1,15 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  private _lifePoints: number;
-  
+  protected _lifePoints: number;
+
   private _strength: number;
   
   constructor() {
     this._lifePoints = 85;
     this._strength = 63;
   }
-
+  
   public get lifePoints(): number {
     return this._lifePoints;
   }
